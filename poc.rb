a0454dcb4e43f9472f85b56de4f87c4edb4ac2e0
Dave Goerlich <dgoerlich@designinginteractive.com>
2015-05-08 10:08:51 -0400

    Different Geoloc functionality

--- poc.rb
+++ poc.rb
@@ -5,19 +5,19 @@ require_relative 'arduino'
 
 ip = open( 'http://jsonip.com/ ' ){ |s| JSON::parse( s.string())['ip'] }
 
+uri = URI.parse("http://ip-api.com/json/#{ip}")
+geoloc = JSON::parse(uri.read)
 
-decoder = Geocoder::Lookup.get :freegeoip
-result = decoder.search(ip).first
+lat = geoloc["lat"]
+lon = geoloc["lon"]
 
-lat = result.data["latitude"]
-lon = result.data["longitude"]
+ip_city = [geoloc["city"], geoloc["region"]].join(", ")
 
 uri = URI.parse("http://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{lon}&mode=json&units=imperial")
 current = JSON::parse(uri.read)
 
-city_name =  current["name"]
 current_temp = current["main"]["temp"]
-
+current_humidity = current["main"]["humidity"]
 
 a = Arduino.new
 inside = a.get_data
@@ -27,10 +27,10 @@ humidity = inside[0]
 temp = ((inside[1].to_f * 9) / 5) + 32
 
 puts "Current IP: #{ip}"
-puts "Location: #{lat} #{lon}"
-puts "City: #{city_name}"
+puts "Location: #{ip_city}: #{lat}, #{lon}"
 
 puts "Outside Temp: #{current_temp} deg"
+puts "Outside Humidity: #{current_humidity} %"
 
-puts "Inside Humidity: #{humidity}%"
 puts "Inside Temp: #{temp} deg"
+puts "Inside Humidity: #{humidity}%"
